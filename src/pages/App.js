a93ed4be16d9a36a993eb569f67d195057a6e9c9
Jason Hargrove <rijaha@gmail.com>
2021-11-18 20:50:34 -0600

    Added .scss filses and updated index.html in src folder.

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,9 +3,12 @@ import React, { useState } from 'react';
 export default function App(props) {
 	return (
 		<div className="AppPage">
-			I changed this on the {props.page} page just to check it out. And styled
-			it!
+			<h1>
+				I changed this on the {props.page} page just to check it out. And styled
+				it!
+			</h1>
 			<p>Hi there Jessalynn. This app and Sass must be working!</p>
+			<button className="btn-main">Hi</button>
 		</div>
 	);
 }
